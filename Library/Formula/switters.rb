b617391bd2fb696c2eebfd7dbd7899278fdb98df
Jim Kubicek <jkubicek@gmail.com>
2015-02-08 16:09:31 -0800

    Formula now creates executable that works

---
+++ Library/Formula/switters.rb
@@ -0,0 +1,45 @@
+class Switters < Formula
+  homepage "https://github.com/jkubicek/switters"
+  url "https://github.com/jkubicek/Switters/archive/0.1.2.tar.gz"
+  sha1 "de75246b72f46e151beef42adb7eb9d6137086f7"
+  version "0.1.2"
+  depends_on :python if MacOS.version <= :snow_leopard
+  
+  resource "tweetpony" do
+  	url "https://pypi.python.org/packages/source/T/TweetPony/tweetpony-1.5.0.tar.gz"
+  	md5 "37e8822be811e7220a80ebb43ccdeb19"
+  end
+  
+  resource "qrcode" do
+  	url "https://pypi.python.org/packages/source/q/qrcode/qrcode-5.1.tar.gz"
+  	md5 "1f20223419bbf992208ada0c12ed4577"
+  end
+  
+  resource "requests" do
+  	url "https://pypi.python.org/packages/source/r/requests/requests-2.5.1.tar.gz"
+  	md5 "c270eb5551a02e8ab7a4cbb83e22af2e"
+  end
+
+  def install
+	ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    %w[tweetpony qrcode requests].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    
+	prefix.install Dir["switterslib"]
+	prefix.install Dir["zxing"]
+	prefix.install Dir["zxing_java"]
+	bin.install "switters"
+
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    system "#{bin}/switters", "-v"
+  end
+end
