3830839d956c57df8076c060abc5b75c72a08bb5
Jim Kubicek <jkubicek@gmail.com>
2015-02-16 12:20:55 -0800

    Use SHA instead of MD5

--- Library/Formula/switters.rb
+++ Library/Formula/switters.rb
@@ -2,22 +2,22 @@ class Switters < Formula
   homepage "https://github.com/jkubicek/switters"
   url "https://github.com/jkubicek/Switters/archive/0.1.2.tar.gz"
   sha1 "de75246b72f46e151beef42adb7eb9d6137086f7"
-  version "0.1.2"
+  version "0.1.3"
   depends_on :python if MacOS.version <= :snow_leopard
   
   resource "tweetpony" do
-  	url "https://pypi.python.org/packages/source/T/TweetPony/tweetpony-1.5.0.tar.gz"
-  	md5 "37e8822be811e7220a80ebb43ccdeb19"
+  	url "https://pypi.python.org/packages/source/T/TweetPony/tweetpony-1.4.2.tar.gz"
+  	sha1 "f00ee5c9f5c8e01abb5af026125690a0eee1b705"
   end
   
   resource "qrcode" do
   	url "https://pypi.python.org/packages/source/q/qrcode/qrcode-5.1.tar.gz"
-  	md5 "1f20223419bbf992208ada0c12ed4577"
+  	sha1 "686d017fb655fa2f1d600b1a8672f5ee538ec2d6"
   end
   
   resource "requests" do
   	url "https://pypi.python.org/packages/source/r/requests/requests-2.5.1.tar.gz"
-  	md5 "c270eb5551a02e8ab7a4cbb83e22af2e"
+  	sha1 "f906c441be2f0e7a834cbf701a72788d3ac3d144"
   end
 
   def install
